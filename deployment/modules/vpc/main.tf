9816119c377dcbea9d18e3514bfbf66da48dc775
mac <mharrvic@gmail.com>
2022-12-27 13:14:24 +0800

    Add VPC global address

--- deployment/modules/vpc/main.tf
+++ deployment/modules/vpc/main.tf
@@ -16,6 +16,25 @@ module "vpc_network" {
     ]
 }
 
+resource "google_compute_global_address" "backstage_private_ip_address" {
+  provider = google-beta
+
+  project       = var.project_id
+  name          = "backstage-private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = "projects/${var.project_id}/global/networks/${module.vpc_network.network_name}"
+}
+
+resource "google_service_networking_connection" "backstage_private_vpc_connection" {
+  provider = google-beta
+
+  network                 = "projects/${var.project_id}/global/networks/${module.vpc_network.network_name}"
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.backstage_private_ip_address.name]
+}
+
 resource "google_vpc_access_connector" "connector" {
   name          = "backstage-connector"
   project       = var.project_id
--- deployment/modules/vpc/outputs.tf
+++ deployment/modules/vpc/outputs.tf
@@ -1,4 +1,4 @@
-output "network" {
+output "vpc_network" {
   value       = module.vpc_network
   description = "Backstage VPC Network"
 }
@@ -6,4 +6,9 @@ output "network" {
 output "backstage_serverless_vpc_connector_name" {
   value       = google_vpc_access_connector.connector.name
   description = "Backstage Serverless VPC Connector"
+}
+
+output "backstage_private_vpc_connection" {
+  value       = google_service_networking_connection.backstage_private_vpc_connection
+  description = "Backstage Private VPC Connection"
 }

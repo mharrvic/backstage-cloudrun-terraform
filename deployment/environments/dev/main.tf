eea188bcf3e4b542f6e09ee024936b06505d3c06
mac <mharrvic@gmail.com>
2022-12-27 13:18:16 +0800

    Add secrets and cloudrun

--- deployment/environments/dev/main.tf
+++ deployment/environments/dev/main.tf
@@ -13,16 +13,42 @@ module "vpc" {
 }
 
 module "iam" {
-  source       = "../../modules/iam"
-  project_id   = var.project
+  source     = "../../modules/iam"
+  project_id = var.project
 }
 
 module "cloudsql" {
-  source            = "../../modules/cloudsql"
-  region            = var.region
-  project_id        = var.project
-  network_id        = "projects/${var.project}/global/networks/${module.vpc.vpc_network.network_name}"
+  source        = "../../modules/cloudsql"
+  region        = var.region
+  project_id    = var.project
+  network_id    = "projects/${var.project}/global/networks/${module.vpc.vpc_network.network_name}"
+  user_password = ""
   depends_on = [
-    module.vpc
+    module.vpc, module.vpc.backstage_private_vpc_connection
+  ]
+}
+
+module "secrets" {
+  source            = "../../modules/secrets"
+  postgres_password = module.cloudsql.generated_user_password
+  depends_on = [
+    module.cloudsql
+  ]
+}
+
+module "cloudrun" {
+  source                            = "../../modules/cloudrun"
+  project_id                        = var.project
+  region                            = var.region
+  vpc_connector_name                = module.vpc.backstage_serverless_vpc_connector_name
+  artifact_registry_url             = var.artifact_registry_url
+  artifact_repo                     = var.artifact_registry_repository
+  cloudsql_instance_name            = module.cloudsql.sql_instance_name
+  cloudsql_instance_connection_name = module.cloudsql.sql_instance_connection_name
+  service_account_email             = module.iam.backstage_service_account_email
+
+
+  depends_on = [
+    module.cloudsql, module.vpc, module.secrets
   ]
 }
